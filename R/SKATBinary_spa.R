307b456f87c875d6d770f4f9d2c475e16b4059d9
ZhangchenZhao <zczhao@umich.edu>
2019-03-30 17:30:08 -0400

    Update SKATBinary_spa.R

--- R/SKATBinary_spa.R
+++ R/SKATBinary_spa.R
@@ -1,3 +1,25 @@
+Beta_Weight<-function(MAF,weights.beta){
+
+	n<-length(MAF)
+	weights<-rep(0,n)	
+	IDX_0<-which(MAF == 0)
+	if(length(IDX_0) == n){
+		stop("No polymorphic SNPs")
+	} else if( length(IDX_0) == 0){
+		weights<-dbeta(MAF,weights.beta[1],weights.beta[2])
+	} else {
+		weights[-IDX_0]<-dbeta(MAF[-IDX_0],weights.beta[1],weights.beta[2])
+	}
+
+	
+	#print(length(IDX_0))
+	#print(weights[-IDX_0])
+	return(weights)
+	
+}
+
+
+
 SPA_ER_kernel<-function(G, obj, res_all, u, Cutoff, MAFsum,qtemp,resampling_time,variancematrix, weight){
 
 	
