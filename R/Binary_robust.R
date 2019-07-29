e0d491d9986f70c992fd8bd4579b2fbeeb06c5f4
ZhangchenZhao <zczhao@umich.edu>
2019-07-29 12:37:23 -0400

    Create Binary_robust.R

---
+++ R/Binary_robust.R
@@ -0,0 +1,8 @@
+	SKATBinary_Robust<-function(Z, obj, kernel = "linear.weighted", method="SKATO"
+	, weights.beta.rare=c(1,25), weights.beta.common=c(0.5,0.5), weights = NULL
+	, r.corr.rare=0, r.corr.common=0, CommonRare_Cutoff=NULL, impute.method = "bestguess"
+	, is_check_genotype=TRUE, is_dosage = FALSE, missing_cutoff=0.15, max_maf=1
+	, estimate_MAF=1){}
+  
+  
+  
