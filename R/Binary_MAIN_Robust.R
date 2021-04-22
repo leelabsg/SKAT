7e69e9266b8fb5bca12d2a8c818081eefad31774
ZhangchenZhao <zczhao@umich.edu>
2021-04-22 18:27:43 -0400

    Update Binary_MAIN_Robust.R

--- R/Binary_MAIN_Robust.R
+++ R/Binary_MAIN_Robust.R
@@ -305,7 +305,7 @@ SKATBinary_Robust.SSD.All = function(SSD.INFO, obj, ...,obj.SNPWeight=NULL){
       warning(msg,call.=FALSE)
       
     } else {
-      if (min(re$mac,0,na.rm=T)>=1){
+      if (min(try1$mac,0,na.rm=T)>=1){
         re =try1
         OUT.Pvalue[i]<-re$p.value
         OUT.Marker[i]<-re$param$n.marker
